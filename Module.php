8c0e088c753ea31fb58ba47353bba63e2f897cf7
Marcin Wiatr <marcin@umotif.com>
2019-02-03 21:08:15 +0000

    Updated implementation of the cache

--- Module.php
+++ Module.php
@@ -1,24 +1,21 @@
 <?php
- 
-/**
- * @link http://www.matacms.com/
- * @copyright Copyright (c) 2015 Qi Interactive Limited
- * @license http://www.matacms.com/license/
- */
 
 namespace matacms\cache;
 
 use mata\base\Module as BaseModule;
 
-class Module extends BaseModule {
+class Module extends BaseModule
+{
 
-	const KV_LAST_MATA_UPDATE_KEY = "matacms\cache::last-mata-update";
+    const KV_LAST_MATA_UPDATE_KEY = "matacms-cache::last-mata-update";
 
-	public function getNavigation() {
-		return null;
-	}
-	
-	public function canShowInNavigation() {
-		return false;
-	}
+    public function getNavigation()
+    {
+        return null;
+    }
+
+    public function canShowInNavigation()
+    {
+        return false;
+    }
 }
--- caching/MataLastUpdatedTimestampDependency.php
+++ caching/MataLastUpdatedTimestampDependency.php
@@ -1,42 +1,14 @@
 <?php
- 
-/**
- * @link http://www.matacms.com/
- * @copyright Copyright (c) 2015 Qi Interactive Limited
- * @license http://www.matacms.com/license/
- */
 
 namespace matacms\cache\caching;
 
-use Yii;
-use yii\caching\Dependency;
-use mata\keyvalue\models\KeyValue;
-use yii\db\Query;
+use yii\caching\DbDependency;
 
-class MataLastUpdatedTimestampDependency extends Dependency {
+class MataLastUpdatedTimestampDependency extends DbDependency
+{
 
-	private static $lastTimestamp;
+    public $sql = "select `Value` from mata_keyvalue where `Key` = :key";
+    public $params = [':key' => \matacms\cache\Module::KV_LAST_MATA_UPDATE_KEY];
+    public $reusable = true;
 
-    protected function generateDependencyData($cache) {
-
-    	if (self::$lastTimestamp)
-    		return self::$lastTimestamp;
-
-    	$query = new Query;
-    	$query->select(['Value'])
-    	    ->from(KeyValue::tableName())
-    	    ->where(['Key' => \matacms\cache\Module::KV_LAST_MATA_UPDATE_KEY]);
-
-    	if (KeyValue::getDb()->enableQueryCache) {
-    	    KeyValue::getDb()->enableQueryCache = false;
-    	    $value = $query->createCommand(KeyValue::getDb())->queryScalar();
-    	    KeyValue::getDb()->enableQueryCache = true;
-    	} else {
-    	    $value = $query->createCommand(KeyValue::getDb())->queryScalar();
-    	}
-
-    	self::$lastTimestamp = $value > 0 ? $value : 1;
-
-        return self::$lastTimestamp;
-    }
 }
